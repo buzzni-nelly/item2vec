1e9e08f382c839247509c194b4b943cbb6582229
nelly <nelly@buzzni.com>
2024-08-16 01:55:18 +0900

    ..

--- queries/__init__.py
+++ queries/__init__.py
@@ -15,7 +15,7 @@ ORDER BY product_id
 """
 
 query_user_items = """
-WITH user_products AS (
+WITH raw_user_products AS (
     SELECT
         event_info.products[1] AS product_id,
         COALESCE(
@@ -30,16 +30,58 @@ WITH user_products AS (
         ) AS uid,
         to_unixtime(time) AS time
     FROM retarget.retarget_access_log
-    WHERE DATE(time) BETWEEN DATE '{start_date}' AND DATE '{end_date}'
+    WHERE DATE(time) = DATE '{date}'
       AND company_id = 'gsshop'
 )
 SELECT
-    u.uid,
-    u.product_id,
-    u.time
-FROM user_products u
-WHERE uid IS NOT NULL
-  AND uid != '00000000-0000-0000-0000-000000000000'
-  AND product_id IS NOT NULL
-ORDER BY u.uid, u.time
+    DISTINCT u.uid AS uid,
+    u.product_id AS product_id,
+    MIN(u.time) AS time
+FROM raw_user_products u
+WHERE NULLIF(u.uid, '') IS NOT NULL
+  AND NULLIF(u.product_id, '') IS NOT NULL
+  AND u.uid != '00000000-0000-0000-0000-000000000000'
+GROUP BY u.uid, u.product_id
+ORDER BY uid, time
+"""
+
+query_joined_items = """
+WITH ranked_products AS (
+  SELECT
+    product_id,
+    click_count,
+    DENSE_RANK() OVER (ORDER BY product_id) - 1 AS pid
+  FROM
+    (
+      SELECT
+        event_info.products [1] AS product_id,
+        COUNT(event_info.products [1]) AS click_count
+      FROM
+        retarget.retarget_access_log
+      WHERE
+        DATE(time) BETWEEN DATE '{start_date}' AND DATE '{end_date}'
+        AND event_info.products [1] IS NOT NULL
+        AND event_info.products [1] != ''
+        AND company_id = 'gsshop'
+      GROUP BY
+        event_info.products [1]
+      HAVING 
+        COUNT(*) >= 20
+    )
+)
+SELECT
+  rp.pid,
+  rp.product_id,
+  rp.click_count,
+  ep.mall_product_name,
+  ep.mall_product_category1,
+  ep.mall_product_category2,
+  ep.mall_product_category3
+FROM
+  ranked_products rp
+  LEFT JOIN iceberg_search.search_ep ep ON rp.product_id = ep.mall_product_id
+WHERE
+  ep.mall_id = 'gsshop'
+ORDER BY
+  rp.pid
 """
