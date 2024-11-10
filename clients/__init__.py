a3a807872ded2ef9d9e9171905579c5ef35e3e10
nelly <nelly@buzzni.com>
2024-11-10 12:44:25 +0900

    database

--- clients/__init__.py
+++ clients/__init__.py
@@ -2,5 +2,6 @@ from clients import ceph
 from clients import trinox
 from clients import mongo
 from clients import redis
+from clients import sqlite3
 
-__all__ = ["ceph", "mongo", "trinox", "redis"]
+__all__ = ["ceph", "mongo", "trinox", "redis", "sqlite3"]
