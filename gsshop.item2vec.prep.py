13085659116a252d75dbb3700e4347aee13fe7c5
justinbuzzni <justin@buzzni.com>
2024-12-10 14:47:20 +0900

    ..

--- gsshop.item2vec.prep.py
+++ gsshop.item2vec.prep.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     volume.migrate_items()
     volume.migrate_users()
     volume.generate_sequential_pairs_csv()
-    volume.generate_click_purchase_footstep_csv(begin_date=datetime.now() - timedelta(days=7))
-    volume.generate_click_click_footstep_csv(begin_date=datetime.now() - timedelta(days=4))
+    volume.generate_click_purchase_footstep_csv(begin_date=datetime.now() - timedelta(days=2))
+    volume.generate_click_click_footstep_csv(begin_date=datetime.now() - timedelta(days=1))
     volume.generate_edge_indices_csv()
