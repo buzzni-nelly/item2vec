a3c575ef8a0c5ef86336ad3b64e169484a8814c1
justinbuzzni <justin@buzzni.com>
2024-12-17 21:39:08 +0900

    ...

--- aboutpet.item2vec.v1.delete.py
+++ aboutpet.item2vec.v1.delete.py
@@ -1,14 +1,15 @@
-import pathlib
-
 import directories
 from item2vec.configs import Settings
+from item2vec.volume import Volume
 
 settings = Settings.load(directories.config("aboutpet", "item2vec", "v1"))
 
 
 def delete_checkpoints():
-    directory = pathlib.Path(settings.checkpoint_dirpath)
-    for file in directory.glob("*"):
+    volume = Volume(company_id="aboutpet", model="item2vec", version="v1")
+    item2vec_checkpoint_dir_path = volume.workspace_path.joinpath("checkpoints")
+    print(f"Removing checkpoints from {item2vec_checkpoint_dir_path}...")
+    for file in item2vec_checkpoint_dir_path.glob("*"):
         if file.is_file():
             file.unlink()
 
