88a72160b4f9725762a386d4613fecb942c5d00f
justinbuzzni <justin@buzzni.com>
2024-12-17 21:33:10 +0900

    bye ceph

--- clients/__init__.py
+++ clients/__init__.py
@@ -1,6 +1,5 @@
-from clients import ceph
 from clients import trinox
 from clients import mongo
 from clients import redis
 
-__all__ = ["ceph", "mongo", "trinox", "redis"]
+__all__ = ["mongo", "trinox", "redis"]
