c4231af02f1152dc55b6c35d23e8610ad491b8f3
justinbuzzni <justin@buzzni.com>
2024-12-22 15:41:01 +0900

    gsshop migrator updated

--- gsshop.item2vec.v1.prep.py
+++ gsshop.item2vec.v1.prep.py
@@ -5,9 +5,12 @@ from item2vec.volume import Migrator
 
 if __name__ == "__main__":
     migrator = Migrator(company_id="gsshop", model="item2vec", version="v1")
-    migrator.migrate_traces(begin_date=datetime(2024, 11, 29))
+    migrator.migrate_traces(begin_date=datetime.now() - timedelta(days=13))
     migrator.migrate_items()
     migrator.migrate_users()
-    migrator.migrate_skip_grams()
-    migrator.migrate_click2purchase_sequences(begin_date=datetime.now() - timedelta(days=2))
+    migrator.migrate_categories()
+    migrator.migrate_skip_grams(window_size=3)
+    migrator.migrate_click2purchase_sequences(begin_date=datetime.now() - timedelta(days=3))
+    migrator.migrate_training_user_histories()
+    migrator.migrate_test_user_histories()
     migrator.generate_edge_indices_csv()
