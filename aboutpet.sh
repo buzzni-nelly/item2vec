1c8532f9c681906e314a70bb1a41804d01180e53
justinbuzzni <justin@buzzni.com>
2024-12-25 01:53:18 +0900

    ..

--- aboutpet.sh
+++ aboutpet.sh
@@ -1,7 +1,7 @@
 while true; do
     python aboutpet.item2vec.v1.prep.py
     sleep 3
-    python aboutpet.popular.py
+    python aboutpet.popular.export.py
     sleep 3
     python aboutpet.item2vec.v1.delete.py
     sleep 3
@@ -14,5 +14,7 @@ while true; do
     python aboutpet.carca.v1.train.py
     sleep 3
     python aboutpet.carca.v1.export.py
+    sleep 3
+    python aboutpet.onnx.validation.py
     sleep 3
 done
