3bf3dbca3f873099c27171a5ad2b672e049807f0
justinbuzzni <justin@buzzni.com>
2025-01-03 15:15:51 +0900

    aboutpet.sh 주석 추가.

--- aboutpet.sh
+++ aboutpet.sh
@@ -7,6 +7,7 @@ while true; do
     sleep 3
     python exec.item2vec.train.py --company-id aboutpet --version v1
     sleep 3
+    # 어바웃펫은 카테고리 검수가 필수 이므로 독립적인 export 사용.
     python aboutpet.item2vec.export.py --company-id aboutpet --version v1
     sleep 3
     python exec.carca.delete.py --company-id aboutpet --version v1
